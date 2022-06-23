2ed82b57ed4616e7fd2bfdc3ad5ccf5dc8a5e6ff
atpham496 <anhthu.pham@sjsu.edu>
2022-06-23 08:25:15 -0700

    minor fixes on overdue check

--- src/component/todos.js
+++ src/component/todos.js
@@ -13,7 +13,9 @@ const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       let color = "white";
-      if (new Date(todo.due) < new Date()) {
+      
+      // overdue tasks
+      if (new Date(todo.due) <= new Date()) {
         color = "red";
       }
 
